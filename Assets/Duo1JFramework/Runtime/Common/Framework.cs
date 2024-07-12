62e5fb674fd30ebb8ced62cb4f0cc2b80930bc3b
Duo1J <770291799@qq.com>
2024-07-12 17:38:09 +0800

    Update

--- Assets/Duo1JFramework/Runtime/Common/Framework.cs
+++ Assets/Duo1JFramework/Runtime/Common/Framework.cs
@@ -4,19 +4,22 @@ namespace Duo1JFramework
 {
     public static class Framework
     {
-        private static bool init = false;
+        /// <summary>
+        /// 框架是否已初始化
+        /// </summary>
+        public static bool Initialized { get; private set; } = false;
 
         /// <summary>
         /// 框架初始化
         /// </summary>
         public static void Init()
         {
-            if (init)
+            if (Initialized)
             {
                 Log.ErrorForce($"{Def.FRAME_WORK_NAME} 重复初始化");
                 return;
             }
-            init = true;
+            Initialized = true;
 
             Log.Info($"{Def.FRAME_WORK_NAME} 初始化开始");
 
@@ -31,6 +34,14 @@ namespace Duo1JFramework
                 Log.ErrorForce($"{Def.FRAME_WORK_NAME} 初始化异常");
                 Assert.ExceptHandle(e);
             }
+        }
+
+        /// <summary>
+        /// 内存清理
+        /// </summary>
+        public static void GC()
+        {
+            Game.GC();
         }
 
         private static void InitInner()
