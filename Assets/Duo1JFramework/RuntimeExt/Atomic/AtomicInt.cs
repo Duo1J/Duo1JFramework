06c4e35011e6fb8ed4c347db50cb580b415985dc
Duo1J <770291799@qq.com>
2024-07-19 22:28:36 +0800

    Update

---
+++ Assets/Duo1JFramework/RuntimeExt/Atomic/AtomicInt.cs
@@ -0,0 +1,46 @@
+using System.Threading;
+
+namespace Duo1JFramework.Ext.Atomic
+{
+    /// <summary>
+    /// Ô­×ÓÐÔInt
+    /// </summary>
+    public class AtomicInt
+    {
+        private int _value;
+
+        public int Read()
+        {
+            return Thread.VolatileRead(ref _value);
+        }
+
+        public void Write(int value)
+        {
+            Thread.VolatileWrite(ref _value, value);
+        }
+
+        public int Increment()
+        {
+            int initialValue, newValue;
+            do
+            {
+                initialValue = Read();
+                newValue = initialValue + 1;
+            } while (Interlocked.CompareExchange(ref _value, newValue, initialValue) != initialValue);
+
+            return newValue;
+        }
+
+        public int Decrement()
+        {
+            int initialValue, newValue;
+            do
+            {
+                initialValue = Read();
+                newValue = initialValue - 1;
+            } while (Interlocked.CompareExchange(ref _value, newValue, initialValue) != initialValue);
+
+            return newValue;
+        }
+    }
+}
